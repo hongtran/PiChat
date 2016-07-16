14f73b351e3cde481a0f52b34b308541f9dd2be3
Hong Tran <phihong2204@gmail.com>
2016-07-16 21:53:26 +0700

    user can send message for multi recipient

--- Gemfile
+++ Gemfile
@@ -48,3 +48,5 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'bootstrap', '~> 4.0.0.alpha3'
 gem 'rails-assets-tether'
+gem 'rails_12factor', group: 'production'
+gem 'carrierwave'
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,14 +5,19 @@ class MessagesController < ApplicationController
 		@message = Message.new
 	end
 	def create
-		@message = current_user.sent_messages.build(message_params)
-		if @message.save
-			flash[:notice] = "create message success"
-			redirect_to root_path
-		else
-			flash[:error] = "Can not create message"
-			redirect_to new_user_message_path(current_user)
+		@recipients = params[:message][:recipient_id]
+		@title = params[:message][:title]
+		@recipients.each do |r|
+		@message = current_user.sent_messages.build(:recipient_id => r, :title => @title)
+			if @message.save
+				flash[:notice] = "create message success"
+				
+			else
+				flash[:error] = "Can not create message"
+				#redirect_to new_user_message_path(current_user)
+			end
 		end
+		redirect_to root_path
 	end
 
 	def show
